f3b4be700bd06108d74892d4913224d5cf39c23e
Lia <brop0032@algonquinlive.com>
2020-03-12 11:57:20 -0400

    Initial commit BBC News Reader

--- app/src/main/java/com/example/cst2335final/MainActivity.java
+++ app/src/main/java/com/example/cst2335final/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cst2335final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView articleList = findViewById(R.id.articleList);
     }
 }
