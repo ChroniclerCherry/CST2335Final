e7a808ef585b0f5aebe85c1b7f0f561340250aa3
karlrez <karlrez123@gmail.com>
2020-03-29 22:15:14 -0400

    Changed the layouts

--- app/src/main/java/com/example/cst2335final/MainActivity.java
+++ app/src/main/java/com/example/cst2335final/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cst2335final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Go to NASA Earthy Image Db activity
+        Button earthyImageBtn = findViewById(R.id.karls_activity_button);
+        Intent goToEarthyImageDb = new Intent(this, Nasa_Earthy_Image_Db.class);
+        earthyImageBtn.setOnClickListener( click -> startActivity(goToEarthyImageDb));
     }
 }
